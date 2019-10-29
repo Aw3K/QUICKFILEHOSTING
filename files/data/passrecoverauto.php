257295ad251dbb7519ba56df19445eeea2ef3ac2
NyggaBytes <37087934+NyggaBytes@users.noreply.github.com>
2019-10-29 15:14:31 +0100

    Update passrecoverauto.php

--- files/data/passrecoverauto.php
+++ files/data/passrecoverauto.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new mysqli('btizszbrodni.nazwa.pl', 'btizszbrodni_accounts', 'AfGi74Koi89', 'btizszbrodni_accounts');
+$conn = new mysqli('accounts');
 $conn -> query("UPDATE `users` SET `passres` = ''");
 ?>
